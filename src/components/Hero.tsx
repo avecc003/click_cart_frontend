2bbc862c087a5c0d8cabf441467ccdf3a2310711
avecc003 <alejandrovecchio01@gmail.com>
2024-06-11 12:27:18 -0400

    hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+
+export default  function Hero(){
+
+    return (
+        <>
+            <img src="../assets/360_F_226543201_ZtKNzyJqxGdW5Lc1louWUd3euRmbjDh9.jpg" alt = 'Hero' 
+            className="w-screen h-[10rem]"> 
+            </img>
+        </>
+    )
+} 
